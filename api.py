55fa082840bc4837a74010952c904bc6b53a0824
Shubam Mehta <s26mehta@gmail.com>
2017-04-02 17:47:48 -0400

    Fixing stuff

--- api.py
+++ api.py
@@ -13,7 +13,8 @@ driver = '{ODBC Driver 13 for SQL Server}'
 
 @app.route('/api/sign_up', methods=['POST'])
 def sign_up():
-    if not request.json:
+    print request.form
+    if not request.form:
         abort(400)
     try:
         cnxn = pyodbc.connect(
@@ -21,11 +22,11 @@ def sign_up():
             ';PWD=' + password)
         cursor = cnxn.cursor()
         cursor.execute("Insert into users(user_name, first_name, last_name, email, password) " \
-                    "VALUES ('%s', '%s', '%s', '%s', '%s')" % (request.json['user_name'], request.json['first_name'],
-                                                     request.json['last_name'], request.json['email'],
-                                                     request.json['password']))
+                    "VALUES ('%s', '%s', '%s', '%s', '%s')" % (request.form['user_name'], request.form['first_name'],
+                                                     request.form['last_name'], request.form['email'],
+                                                     request.form['password']))
         cnxn.commit()
-        cursor.execute("Select id from users where user_name = '%s' and email = '%s' and last_name = '%s'" % (request.json['user_name'], request.json['email'], request.json['last_name']))
+        cursor.execute("Select id from users where user_name = '%s' and email = '%s' and last_name = '%s'" % (request.form['user_name'], request.form['email'], request.form['last_name']))
         row = cursor.fetchone()
         print row
         cursor.execute("Insert into dashboards(user_id, fighters) " \
@@ -33,34 +34,36 @@ def sign_up():
         cnxn.commit()
         cursor.close()
         cnxn.close()
-        return jsonify(success=True)
+        return jsonify(success=True, user_id=row[0])
     except:
         return jsonify(success=False)
 
 
-@app.route('/api/log_in', methods=['GET'])
+@app.route('/api/log_in', methods=['POST'])
 def log_in():
-    if not request.json:
+    print request.form
+    print request.json
+    if not request.form:
         abort(400)
     try:
         cnxn = pyodbc.connect(
             'DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username +
             ';PWD=' + password)
         cursor = cnxn.cursor()
-        cursor.execute("Select user_name, password from users where user_name = '%s' and password = '%s'" % (request.json['user_name'], request.json['password']))
+        cursor.execute("Select id, user_name, password, first_name from users where user_name = '%s' and password = '%s'" % (request.form['user_name'], request.form['password']))
         row = cursor.fetchone()
         print row
         cnxn.commit()
         cursor.close()
         cnxn.close()
-        if row[0] == request.json['username'] and row[1] == request.json['password']:
-            return jsonify(success=True)
+        if row[1] == request.form['username'] and row[2] == request.form['password']:
+            return jsonify(success=True, user_id=row[0], first_name=row[3])
         else:
             return jsonify(success=False)
     except:
         return jsonify(success=False)
 
-@app.route('/api/get_all_fighters', methods=['GET'])
+@app.route('/api/get_all_fighters', methods=['POST'])
 def get_all_fighters():
     try:
         cnxn = pyodbc.connect(
@@ -286,7 +289,5 @@ def live_fight_data():
     except:
         return jsonify(success=False)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
